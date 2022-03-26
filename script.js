0974ab077a06d4991aa1dca026e6f71898c9c1e9
Yana Kuzina <kuzina25.10.2013@gmail.com>
2022-03-26 14:02:51 +0200

    first version HW_12

---
+++ script.js
@@ -0,0 +1,7 @@
+const getUsers = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+  const user = data.find(({ company }) => company.name === "Johns Group");
+  console.log(user);
+};
+getUsers();
